aeabb83d5664680eadfbeef0e0243f11bda248ef
Harry Lincoln <harry.lincoln@edcoms.co.uk>
2016-11-26 21:46:01 +0000

    using service in controller to pull out data

--- src/app/components/determine-group/determine-group.js
+++ src/app/components/determine-group/determine-group.js
@@ -1,6 +1,16 @@
 module.exports = {
-  template: require('./determine-group.html'),
-  controller: function () {
-    this.hello = 'Hello World!';
-  }
+    template: require('./determine-group.html'),
+    controller: function(StepsService) {
+        var ctrl = this;
+        ctrl.hello = 'Hello World!';
+
+        // by $window.url
+        var stepURL = 'interactive/step-1';
+
+        StepsService
+            .getInteractiveGroup(stepURL)
+            .then(function(response) {
+                ctrl.datResponse = response;
+            });
+    }
 };
